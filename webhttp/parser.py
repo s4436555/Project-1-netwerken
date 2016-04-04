70ead65fb511865464b7cd89654c672e0cb1b5d4
s4436555 <j.moerman@student.ru.nl>
2016-04-04 17:49:17 +0200

    3. Implemented "parse_requests"

--- webhttp/parser.py
+++ webhttp/parser.py
@@ -27,6 +27,27 @@ class RequestParser:
         http_requests = []
         for request in requests:
             http_request = webhttp.message.Request()
+            length = len(request)
+            
+            """Parsing start_line"""
+            end_line = request.find('\r\n', 0)
+            http_request.start_line = request[0:end_line]
+            start_line = end_line + 2
+            
+            """Parsing header lines"""
+            while (start_line + 1 < length and
+                   request[start_line] != '\r' and 
+                   request[start_line + 1] != '\n'):
+                end_line = request.find('\r\n', start_line)
+                if end_line < 0:
+                    print("missing clrf")
+                    break
+                colon = request.find(': ', start_line, end_line)
+                http_request.set_header(
+                    request[start_line:colon-1], request[colon+2:end_line]
+                )
+                start_line = end_line + 2
+            
             http_requests.append(http_request)
         
         return http_requests
